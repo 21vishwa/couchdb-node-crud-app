8fb056bfe199e28091de31d7d1c920e7839be9e4
Kasi Vishwanathan <137622399+21vishwa@users.noreply.github.com>
2025-12-30 10:51:57 +0530

    Update cruddb.js

--- db/cruddb.js
+++ db/cruddb.js
@@ -1,8 +1,8 @@
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
-const nano = require("nano")("https://ruler:ruler@192.168.57.254:5984");
+const nano = require("nano")("https://admin:pass@192.168.57.254:5984");
 
 // ✅ DASHBOARD DATABASE
-const cruddb = nano.use("kasi_");
+const cruddb = nano.use("dbname");
 
 module.exports = cruddb;
